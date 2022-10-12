96cd19965369bcc7650276643e00901dcfe545ea
P1ul0 <paulocesar147852@gmail.com>
2022-10-11 21:35:53 -0300

    correção da entidade usuario, pasta usuarioSenha deletada

--- index.js
+++ index.js
@@ -8,54 +8,46 @@ const db = require("./database/configDatabase");
 
 app.use(cors());
 app.use(express.json());
-// app para rota usuarioSenha
-app.use('/usuarioSenha', rotaUsuarioSenha);
 
-app.post('/register', async (req, res) => {
+app.post('/register', (req, res) => {
     const {nome, sobrenome, email, estado, cidade, telefone, nicho} = req.body;
-
     const SQL = "INSERT INTO usuarios2 (nome, sobrenome, email, estado, cidade, telefone, nicho) VALUES (?,?,?,?,?,?,?)";
-    try{
-    await db.query(SQL, [nome, sobrenome, email, estado, cidade, telefone, nicho], (err, result) => {
-        res.status(200).send("usuário adicionado com sucesso")
+
+    db.query(SQL, [nome, sobrenome, email, estado, cidade, telefone, nicho], (err, result) => {
+        if(err) return res.status(500).send(err)
+
+        return res.status(200).send("usuário adicionado com sucesso")
     });
-    }catch (err) {
-       res.status(500).send(err)
-    }
 });
 
-app.get("/getUsuarios", async (req, res) => {
+app.get("/getUsuarios", (req, res) => {
     const SQL = "SELECT * from usuarios2";
     
-   await db.query(SQL, (err, result) => {
+   db.query(SQL, (err, result) => {
+        if(err) return res.status(500).send("erro ao recuperar os usuários");
         
-        if(err) res.status(500).send("erro ao recuperar os usuários");
-        
-        res.status(200).send(result);
+        return res.status(200).send(result);
     });
 });
 
-app.put("/edit", async (req, res) => {
+app.put("/edit", (req, res) => {
     const {nome,sobrenome,email,estado,cidade,telefone,nicho, id_usuario} = req.body;
-    
     const SQL = "UPDATE usuarios2 SET nome = ?, sobrenome = ?, email = ?, estado = ?, cidade = ?, telefone = ?, nicho = ? WHERE id_usuario = " + id_usuario;
 
-    await db.query(SQL,[nome, sobrenome, email, estado, cidade, telefone, nicho, id_usuario], (err, result) => {
+    db.query(SQL,[nome, sobrenome, email, estado, cidade, telefone, nicho, id_usuario], (err, result) => {
+        if(err) return res.status(500).send("usuário não localizado");
         
-        if(err) res.status(500).send("usuário não localizado");
-        
-        res.send(result);
+        return res.send(result);
     });
 });
 
-app.delete("/delete/:id_usuario", async (req, res) => {
+app.delete("/delete/:id_usuario", (req, res) => {
     const {id_usuario} = req.params;
     const SQL = "DELETE FROM usuarios2 WHERE id_usuario = ?";
-    await db.query(SQL, id_usuario, (err, result) => {
-        
+
+    db.query(SQL, id_usuario, (err, result) => {
         if(err) res.status(500).send("usuário não localizado");
-        
-        res.send(result);
+        return res.send(result);
     });
 });
 
